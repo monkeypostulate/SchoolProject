2be7bdc9aece0816b670296dcd40b100e4cee76a
Monkey Postulate <cuatrooctavos@yahoo.com.mx>
2022-09-05 18:22:07 +0200

    course table

---
+++ database/course.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE TABLE 
+school.course
+(
+  CourseId INT,
+  CourseName STRING,
+  StartDate DATE,
+  EndDate DATE,
+  Credit FLOAT64,
+  Capacity FLOAT64,
+  InsertionDate TIMESTAMP
+)
