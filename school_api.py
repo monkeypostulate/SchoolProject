ef473bddd2b402762f18f278b978556d73aa5954
Monkey Postulate <cuatrooctavos@yahoo.com.mx>
2022-09-07 20:04:26 +0200

    Update school_api.py

--- school_api.py
+++ school_api.py
@@ -11,9 +11,6 @@ import pandas
 import json
 
 
-
-
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -29,29 +26,33 @@ def home():
 
 ##############################################
 # Get student course information
-@app.route('/api/v1/resources/student/info', methods=['GET'])
+@app.route('/school-api/v1/student/course', methods=['GET'])
 def get_students_info():
 
-    if 'student_id' in request.args:
-        student_id = int(request.args['student_id'])
+    if 'studentId' in request.args:
+        studentId = int(request.args['studentId'])
     else:
         return """
         <h2> Error: </h2> <p>
-    No student_id field provided. </p>"""
+    No studentId field provided. </p>"""
 
     results = []
     get_courses = SchoolStudent()
-    all_courses =get_courses.student_information(student_id = student_id, student_information = 'all courses')
+    all_courses =get_courses.student_information(student_id = studentId, student_information = 'all courses')
     for i in range(all_courses.shape[0]):
         results.append(json.loads( all_courses.iloc[i].to_json()) )
-    return results
+    if len(results)>0:
+        return results
+    else:
+        return """<h1>Error</h1>
+    <p> No results found </p>"""
 
 # Get student average grade information
-@app.route('/api/v1/resources/student/average-grade', methods=['GET'])
+@app.route('/school-api/v1/student/average-grade', methods=['GET'])
 def get_students_avg_grade():
 
-    if 'student_id' in request.args:
-        student_id = int(request.args['student_id'])
+    if 'studentId' in request.args:
+        studentId = int(request.args['studentId'])
     else:
         return """
         <h2> Error: </h2> <p>
@@ -59,19 +60,19 @@ def get_students_avg_grade():
 
     results = []
     get_courses = SchoolStudent()
-    all_courses =get_courses.student_information(student_id = student_id, student_information = 'student average')
+    all_courses =get_courses.student_information(student_id = studentId, student_information = 'student average')
     for i in range(all_courses.shape[0]):
         results.append(json.loads( all_courses.iloc[i].to_json()) )
     return results
 
 
 # Get student course  grade information
-@app.route('/api/v1/resources/student/course-grade', methods=['GET'])
+@app.route('/school-api/v1/student/course-grade', methods=['GET'])
 def get_students_course_grade():
 
-    if 'student_id' in request.args and 'course_id' in request.args:
-        student_id = int(request.args['student_id'])
-        course_id = int(request.args['course_id'])
+    if 'studentId' in request.args and 'courseId' in request.args:
+        studentId = int(request.args['studentId'])
+        courseId = int(request.args['courseId'])
     else:
         return """
         <h2> Error: </h2> <p>
@@ -80,8 +81,8 @@ def get_students_course_grade():
     results = []
     get_courses = SchoolStudent()
     all_courses =get_courses.student_information(
-        student_id = student_id,
-        course_id = course_id,
+        student_id = studentId,
+        course_id = courseId,
         student_information = 'course grade')
     for i in range(all_courses.shape[0]):
         results.append(json.loads( all_courses.iloc[i].to_json()) )
@@ -90,40 +91,40 @@ def get_students_course_grade():
 
 ##############################################
 # Add a student to the school
-@app.route('/api/v1/resources/student/create', methods=['GET','POST'])
+@app.route('/school-api/v1/student/create', methods=['GET','POST'])
 def set_students_info():
     if 'credit_capacity' in request.args:
-        first_name = request.args['first_name']
-        family_name = request.args['family_name']
-        credit_capacity = int(request.args['credit_capacity'])
+        firstName = request.args['firstName']
+        familyName = request.args['familyName']
+        creditCapacity = int(request.args['creditCapacity'])
     else:
         return "Error: No credit_capacity field provided."
     
-    SchoolStudent.student(first_name = first_name,
-                        family_name = family_name,
-                        credit_capacity = credit_capacity)
+    SchoolStudent.student(first_name = firstName,
+                        family_name = familyName,
+                        credit_capacity = creditCapacity)
     return "<p> Succesfully added a new student  </p>"
 
 
 ##############################################
 # Update student information
-@app.route('/api/v1/resources/student/update',
+@app.route('/school-api/v1/student/update',
            methods=['GET','POST'])
 def update_students_info():
-    if 'student_id' in request.args and 'first_name' in request.args and 'family_name' in request.args:
-        student_id = request.args['student_id']
-        first_name = request.args['first_name']
-        family_name = request.args['family_name']
+    if 'studentId' in request.args and 'firstName' in request.args and 'family_name' in request.args:
+        studentId = request.args['studentId']
+        firstName = request.args['firstName']
+        familyName = request.args['familyName']
     else:
         return """
         <h2> Error:</h2>
         <p>student_id, family_name or first_name information missing</p>"""
     
     SchoolStudent.student(
-        student_id= student_id,
-        first_name = first_name,
-        family_name = family_name,
-        credit_capacity = credit_capacity)
+        student_id= studentId,
+        first_name = firstName,
+        family_name = familyName,
+        credit_capacity = creditCapacity)
     return "<p> Succesfully updated student info.  </p>"
 
 
@@ -131,12 +132,12 @@ def update_students_info():
 
 ##############################################
 # Add a student to the school
-@app.route('/api/v1/resources/course/create', methods=['GET','POST'])
+@app.route('/school-api/v1/course/create', methods=['GET','POST'])
 def set_course_info():
     if 'course_name' in request.args:
-        course_name = request.args['course_name']
-        start_date = request.args['start_date']
-        end_date = request.args['end_date']
+        course_name = request.args['courseName']
+        start_date = request.args['startDate']
+        end_date = request.args['endDate']
         credit = request.args['credit']
         capacity =request.args['capacity']
     else:
@@ -144,9 +145,9 @@ def set_course_info():
         <h2>Error:</h2>
         <p>Information missing.</p>"""
 
-    SchoolCourse.course(course_name = course_name,
-                     start_date = start_date,
-                     end_date = end_date,
+    SchoolCourse.course(course_name = courseName,
+                     start_date = startDate,
+                     end_date = endDate,
                      credit = int(credit),
                      capacity = int(capacity) )
     return "<p> Succesfully added a new course  </p>"
@@ -155,18 +156,18 @@ def set_course_info():
 
 ##############################################
 # Add a student to  course
-@app.route('/api/v1/resources/student2course/insert', methods=['GET','POST'])
+@app.route('/school-api/v1/course/add-student', methods=['GET','POST'])
 def insert_student2course():
-    if 'student_id' in request.args and 'course_id' in request.args:
-        student_id = request.args['student_id']
-        course_id = request.args['course_id']
+    if 'studentId' in request.args and 'courseId' in request.args:
+        studentId = request.args['studentId']
+        courseId = request.args['courseId']
     else:
         return """
         <h2>Error:</h2>
         <p>Information missing.</p>"""
 
-    student2course = StudentInCourse(student_id = student_id )
-    student2course.add_student_to_course( course_id = course_id)
+    student2course = StudentInCourse(student_id = studentId )
+    student2course.add_student_to_course( course_id = courseId)
     return "<p> Succesfully added student to  course  </p>"
 
 
@@ -174,18 +175,18 @@ def insert_student2course():
 
 ##############################################
 # Add student's grade
-@app.route('/api/v1/resources/student-grade/insert', methods=['GET','POST'])
+@app.route('/school-api/v1/course/student-grade/insert', methods=['GET','POST'])
 def add_student_grade():
-    if 'student_id' in request.args and 'course_id' in request.args:
-        student_id = request.args['student_id']
-        course_id = request.args['course_id']
+    if 'studentId' in request.args and 'courseId' in request.args:
+        student_id = request.args['studentId']
+        course_id = request.args['courseId']
         grade = request.args['grade']
     else:
         return """
         <h2>Error:</h2>
         <p>Information missing.</p>"""
     add_grade = Grade(course_id = course_id)
-    add_grade.add(student_id = student_id, grade = grade)
+    add_grade.add(student_id = studentId, grade = grade)
 
     return "<p> Succesfully added student's grade  </p>"
 
